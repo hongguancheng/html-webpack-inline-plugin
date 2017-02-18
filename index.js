cabe20ad39751f4025e5eef756f838d3d93ca886
CCG <guancheng.hong@gmail.com>
2017-02-18 17:51:36 +0800

    兼容 css 文件

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function HtmlWebpackInlinePlugin (opt) {
+function HtmlWebpackInlineChunksPlugin (opt) {
+  // 插件参数，配置的 chunk 将内联到所有 html-webpack-plugin 配置的 html 中
   this.chunks = opt.chunks || []
 }
-HtmlWebpackInlinePlugin.prototype.apply = function (compiler, callback) {
+
+HtmlWebpackInlineChunksPlugin.prototype.apply = function (compiler, callback) {
   compiler.plugin('compilation', compilation => {
     compilation.plugin('html-webpack-plugin-alter-asset-tags', (htmlPluginData, callback) => {
       // 合并去重
@@ -12,24 +14,39 @@ HtmlWebpackInlinePlugin.prototype.apply = function (compiler, callback) {
           return chunk.name === name
         })[0]
 
-        let chunkPath = chunk && chunk.files[0]
-
-        if (!chunkPath) {
-          return
-        }
-
-        htmlPluginData.body.unshift({
-          tagName: 'script',
-          closeTag: true,
-          attributes: {
-            type: 'text/javascript'
-          },
-          innerHTML: compilation.assets[chunkPath].source()
-        })
+        chunk && this.addData(compilation, htmlPluginData, chunk)
       })
       callback(null, htmlPluginData)
     })
   })
 }
 
-module.exports = HtmlWebpackInlinePlugin
+HtmlWebpackInlineChunksPlugin.prototype.addData = function (compilation, htmlPluginData, chunk) {
+  let reCss = /\.css$/
+  let reJs = /\.js$/
+
+  // 判断文件类别，按类型加入到 htmlPluginData 中
+  chunk.files.forEach((file) => {
+    if (reCss.test(file)) {
+      htmlPluginData.body.unshift({
+        tagName: 'style',
+        closeTag: true,
+        attributes: {
+          type: 'text/css'
+        },
+        innerHTML: compilation.assets[file].source()
+      })
+    } else if (reJs.test(file)) {
+      htmlPluginData.body.unshift({
+        tagName: 'script',
+        closeTag: true,
+        attributes: {
+          type: 'text/javascript'
+        },
+        innerHTML: compilation.assets[file].source()
+      })
+    }
+  })
+}
+
+module.exports = HtmlWebpackInlineChunksPlugin
